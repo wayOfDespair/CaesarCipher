e2bd644297cd01d2281c6fb6c27fe5b048bdbd32
alexey <email@email.com>
2020-03-11 19:20:58 +0300

    refactor

---
+++ src/com/company/MainLogic.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class MainLogic {
+
+    public static String encryptText(String str, int shift) {
+        char[] text = str.toUpperCase().toCharArray();
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] < 91 - shift) {
+                text[i] += shift;
+            }
+            else {
+                text[i] = (char) (text[i] - 25 + shift - 1);
+            }
+
+        }
+        return String.valueOf(text);
+    }
+
+    public static String decryptText(String str, int shift) {
+        char[] text = str.toUpperCase().toCharArray();
+        for (int i = 0; i < text.length; i++) {
+            if (text[i] > 64 + shift) {
+                text[i] -= shift;
+            }
+            else {
+                text[i] = (char) (text[i] + 25 - shift + 1);
+            }
+        }
+        return String.valueOf(text);
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class MainWindow extends JFrame {
 
     public MainWindow() throws HeadlessException {
         super("Caesar cipher");
-        setSize(new Dimension(600, 400));
+        setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -22,90 +24,102 @@ public class MainWindow extends JFrame {
         setVisible(true);
     }
 
-    private JTextField originalText = new JTextField();
-    private JTextField morphedText = new JTextField();
-    private JTextField shift = new JTextField();
-    private JButton encryptButton = new JButton("Encrypt");
-    private JButton decryptButton = new JButton("Decrypt");
+
+    private JTextField originalTextField = new JTextField();
+    private JTextField morphedTextField  = new JTextField();
+    private JComboBox<Integer> shift     = new JComboBox<>();
+    private JLabel originalTextLabel     = new JLabel("Original text");
+    private JLabel morphedTextLabel      = new JLabel();
+    private JLabel shiftLabel            = new JLabel("Select shift");
+    private JButton encryptButton        = new JButton("Encrypt");
+    private JButton decryptButton        = new JButton("Decrypt");
+
+    private final int WINDOW_WIDTH    = 550;
+    private final int WINDOW_HEIGHT   = 300;
+    private final int LABEL_WIDTH     = 90;
+    private final int TEXTFIELD_WIDTH = 400;
+    private final int BUTTON_WIDTH    = 165;
+
 
     private void setOriginalText() {
-        originalText.setBounds(new Rectangle(10, 10, 500, 30));
-        add(originalText);
+        originalTextLabel.setBounds(new Rectangle(10, 10, LABEL_WIDTH, 30));
+        originalTextField.setBounds(new Rectangle(100, 10, TEXTFIELD_WIDTH, 30));
+        add(originalTextLabel);
+        add(originalTextField);
     }
 
     private void setMorphedText() {
-        morphedText.setBounds(new Rectangle(10, 90, 500, 30));
-        add(morphedText);
+        morphedTextLabel.setBounds(new Rectangle(10, 90, LABEL_WIDTH, 30));
+        morphedTextField.setBounds(new Rectangle(100, 90, TEXTFIELD_WIDTH, 30));
+        add(morphedTextLabel);
+        add(morphedTextField);
     }
 
     private void setShift() {
-        shift.setBounds(new Rectangle(10, 50, 100, 30));
-        shift.setText(String.valueOf(0));
+        shift.setBounds(new Rectangle(100, 50, 50, 30));
+        shiftLabel.setBounds(new Rectangle(10, 50, LABEL_WIDTH, 30));
+        for (int i = 0; i < 26; i++) {
+            shift.addItem(i);
+        }
+        shift.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                switch (keyEvent.getKeyCode()) {
+                    case KeyEvent.VK_MINUS:
+                        if (shift.getSelectedIndex() > 0) {
+                            shift.setSelectedIndex(shift.getSelectedIndex() - 1);
+                            break;
+                        }
+                    case KeyEvent.VK_EQUALS:
+                        if (shift.getSelectedIndex() < 26) {
+                            shift.setSelectedIndex(shift.getSelectedIndex() + 1);
+                            break;
+                        }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+
+            }
+        });
+        add(shiftLabel);
         add(shift);
     }
 
     private void setEncryptButton() {
-        encryptButton.setBounds(new Rectangle(210, 50, 100, 30));
-        encryptButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                originalText.setText(originalText.getText().toUpperCase());
-                if (isNumeric(shift.getText())) {
-                    morphedText.setText(encryptText(originalText.getText(), Integer.parseInt(shift.getText())));
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "Enter integer in shift field");
-                }
+        encryptButton.setBounds(new Rectangle(160, 50, BUTTON_WIDTH, 30));
+        encryptButton.addActionListener(actionEvent -> {
+            originalTextField.setText(originalTextField.getText().toUpperCase());
+            if (MainLogic.isNumeric(String.valueOf(shift.getSelectedItem()))) {
+                morphedTextField.setText(MainLogic.encryptText(originalTextField.getText(), Integer.parseInt(String.valueOf(shift.getSelectedItem()))));
+                morphedTextLabel.setText("Encrypted text");
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Enter integer in shift field");
             }
         });
         add(encryptButton);
     }
 
     private void setDecryptButton() {
-        decryptButton.setBounds(new Rectangle(320, 50, 100, 30));
-        decryptButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                originalText.setText(originalText.getText().toUpperCase());
-                if (isNumeric(shift.getText())) {
-                    morphedText.setText(decryptText(originalText.getText(), Integer.parseInt(shift.getText())));
-                }
-                else {
-                    JOptionPane.showMessageDialog(null, "Enter integer in shift field");
-                }
+        decryptButton.setBounds(new Rectangle(335, 50, BUTTON_WIDTH, 30));
+        decryptButton.addActionListener(actionEvent -> {
+            originalTextField.setText(originalTextField.getText().toUpperCase());
+            if (MainLogic.isNumeric(String.valueOf(shift.getSelectedItem()))) {
+                morphedTextField.setText(MainLogic.decryptText(originalTextField.getText(), Integer.parseInt(String.valueOf(shift.getSelectedItem()))));
+                morphedTextLabel.setText("Decrypted text");
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Enter integer in shift field");
             }
         });
         add(decryptButton);
-    }
-
-    private String encryptText(String text, int shift) {
-        char[] origText = text.toUpperCase().toCharArray();
-        for (int i = 0; i < origText.length; i++) {
-            if (origText[i] < 91 - shift) origText[i] += shift;
-            else origText[i] = (char) (origText[i] - 25 + shift - 1);
-
-        }
-        return String.valueOf(origText);
-    }
-
-    private String decryptText(String text, int shift) {
-        char[] origText = text.toUpperCase().toCharArray();
-        for (int i = 0; i < origText.length; i++) {
-            if (origText[i] > 64 + shift) {
-                origText[i] -= shift;
-            }
-            else origText[i] = (char) (origText[i] + 25 - shift + 1);
-        }
-        return String.valueOf(origText);
-    }
-
-    private boolean isNumeric(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch(NumberFormatException e){
-            return false;
-        }
     }
 
 }
